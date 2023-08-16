7352723baa6c2c1389558bbde2ec29eaa02cd7d0
Hemanth B <hemanth22hemu@gmail.com>
2023-08-16 07:32:13 +0530

    feat: daily telegram notification

---
+++ thursday_bot.py
@@ -0,0 +1,20 @@
+import requests
+import os
+
+bot_token = os.environ.get('TELE_TOKEN')
+
+#message = "Hello World!"
+
+with open('thursday.txt','r') as file:
+    file_contents = file.read()
+
+message = file_contents
+
+
+def telegram_send_message(message):
+    url = "https://api.telegram.org/bot{}/sendMessage?chat_id=1288523002&text={}".format(bot_token, message)
+    requests.get(url)
+
+#telegram_send_message(message)
+for new_sendMessage_tele in message.split("\n"):
+    telegram_send_message(new_sendMessage_tele)
