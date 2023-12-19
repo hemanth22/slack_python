64c4c4a63485960afb4dad635346baeeaeb4fec3
Hemanth B <hemanth22hemu@gmail.com>
2023-12-19 21:05:35 +0530

    feat: adding news python

---
+++ telegram_bot_bb_all.py
@@ -0,0 +1,21 @@
+import requests
+import os
+
+bot_token = os.environ.get('TELE_TOKEN')
+
+#message = "Hello World!"
+
+with open('bloomberg_all_news.txt','r') as file:
+    file_contents = file.read()
+
+message = file_contents
+
+
+def telegram_send_message(message):
+    url = "https://api.telegram.org/bot{}/sendMessage?chat_id=1288523002&text={}".format(bot_token, message)
+    requests.get(url)
+
+#telegram_send_message(message)
+
+for new_sendMessage_tele in message.split("\n"):
+    telegram_send_message(new_sendMessage_tele)
